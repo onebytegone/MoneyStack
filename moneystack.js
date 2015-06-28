a3803b91457e3d2efd81d0c51271e63ae6c32db0
Ethan Smith <ethan@onebytegone.com>
2015-06-27 20:36:56 -0400

    Adjusted value capping
    When using .toFixed() there was rounding issues. So thus the move away from that.

--- moneystack.js
+++ moneystack.js
@@ -13,18 +13,29 @@
 
    MoneyStack.prototype.__upscaleValue = function(value, precision) {
       var adjusted = value * this.__determineScale(precision);
-      return Number(adjusted.toFixed(0));
+      return this.__capToInterger(adjusted);
    };
 
 
    MoneyStack.prototype.__downscaleValue = function(value, precision) {
       var adjusted = value / this.__determineScale(precision);
-      return Number(adjusted.toFixed(precision));
+      return this.__truncateValue(adjusted, precision);
    };
 
 
    MoneyStack.prototype.__determineScale = function(precision) {
       return Math.pow(10, precision);
+   };
+
+
+   MoneyStack.prototype.__truncateValue = function(value, precision) {
+      var scale = this.__determineScale(precision);
+      return this.__capToInterger( value * scale ) / scale;
+   };
+
+
+   MoneyStack.prototype.__capToInterger = function(value) {
+      return value > 0 ? Math.floor(value) : Math.ceil(value);
    };
 
 
--- tests/moneystack-scaling.test.js
+++ tests/moneystack-scaling.test.js
@@ -31,6 +31,10 @@ describe('Value Scaling', function() {
          expect(stack.__downscaleValue(1200, 2)).to.be(12);
          expect(stack.__downscaleValue(4223, 2)).to.be(42.23);
          expect(stack.__downscaleValue(42230, 3)).to.be(42.23);
+         expect(stack.__downscaleValue(10.99, 0)).to.be(10);
+         expect(stack.__downscaleValue(109.3, 1)).to.be(10.9);
+         expect(stack.__downscaleValue(1099, 2)).to.be(10.99);
+         expect(stack.__downscaleValue(1099.33, 2)).to.be(10.99);
       });
       it('should truncate to the given precision', function() {
          expect(stack.__downscaleValue(42.2312312424, 0)).to.be(42);
@@ -46,6 +50,43 @@ describe('Value Scaling', function() {
          expect(stack.__determineScale(3)).to.be(1000);
          expect(stack.__determineScale(4)).to.be(10000);
          expect(stack.__determineScale(5)).to.be(100000);
+      });
+   });
+   describe('#__truncateValue(value, precision)', function() {
+      it('should trim decimal to given length', function() {
+         expect(stack.__truncateValue(0, 0)).to.be(0);
+         expect(stack.__truncateValue(5, 0)).to.be(5);
+         expect(stack.__truncateValue(5.4, 0)).to.be(5);
+         expect(stack.__truncateValue(5.4, 1)).to.be(5.4);
+         expect(stack.__truncateValue(5.4, 2)).to.be(5.4);
+         expect(stack.__truncateValue(5.40003, 2)).to.be(5.4);
+         expect(stack.__truncateValue(5.99, 2)).to.be(5.99);
+         expect(stack.__truncateValue(5.9999, 2)).to.be(5.99);
+         expect(stack.__truncateValue(10.99, 1)).to.be(10.9);
+         expect(stack.__truncateValue(10.99, 2)).to.be(10.99);
+      });
+   });
+   describe('#__capToInterger(value)', function() {
+      it('should cap positive numbers to lower bound', function() {
+         expect(stack.__capToInterger(0)).to.be(0);
+         expect(stack.__capToInterger(0.005)).to.be(0);
+         expect(stack.__capToInterger(1)).to.be(1);
+         expect(stack.__capToInterger(5)).to.be(5);
+         expect(stack.__capToInterger(5.0000000001)).to.be(5);
+         expect(stack.__capToInterger(5.1)).to.be(5);
+         expect(stack.__capToInterger(5.5)).to.be(5);
+         expect(stack.__capToInterger(5.9)).to.be(5);
+         expect(stack.__capToInterger(5.9999999999)).to.be(5);
+      });
+      it('should cap negative numbers to upper bound', function() {
+         expect(stack.__capToInterger(-1)).to.be(-1);
+         expect(stack.__capToInterger(-0.005)).to.be(0);
+         expect(stack.__capToInterger(-5)).to.be(-5);
+         expect(stack.__capToInterger(-5.0000000001)).to.be(-5);
+         expect(stack.__capToInterger(-5.1)).to.be(-5);
+         expect(stack.__capToInterger(-5.5)).to.be(-5);
+         expect(stack.__capToInterger(-5.9)).to.be(-5);
+         expect(stack.__capToInterger(-5.9999999999)).to.be(-5);
       });
    });
 });
